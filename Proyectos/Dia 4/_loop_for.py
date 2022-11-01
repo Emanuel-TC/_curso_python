97847060b3b9b99d6d4ce6cabf93968c2fb5aaf7
Emanuel-TC <heberemanuelroma@gmail.com>
2022-10-31 18:07:06 -0600

    correcion_comentarios

--- Proyectos/Dia 4/_loop_for.py
+++ Proyectos/Dia 4/_loop_for.py
@@ -46,9 +46,9 @@ for numero in lista_numeros:
 # suma_pares y suma_impares respectivamente: lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
 #*Recordando de los días anteriores: el módulo (o resto) de un número dividido 2 es cero cuando dicho valor es par, y 1 cuando es impar
 
-num % 2 == 0 (valores pares)
+#num % 2 == 0 (valores pares)
 
-num % 2 == 1 (valores impares)
+#num % 2 == 1 (valores impares)
 
 lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
 suma_pares = 0
