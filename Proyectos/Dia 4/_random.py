537d1c4fd7acd9a91ba5dfbf63fdb1d60bb11340
Emanuel-TC <heberemanuelroma@gmail.com>
2022-11-02 20:58:04 -0600

    _random_ejercicios

--- Proyectos/Dia 4/_random.py
+++ Proyectos/Dia 4/_random.py
@@ -22,3 +22,24 @@ print(aleatorio)
 numeros = list(range(5,50,5))
 shuffle(numeros)
 print(numeros)
+
+#Implementa la función randint() de la librería random que te permita obtener un número entero del 1 al 10,
+# y almacena dicho valor en una variable llamada aleatorio
+
+#from random import randint
+aleatorio = randint(1,10)
+
+#Práctica Random 2
+#Implementa la función random() de la librería random que te permita obtener un número decimal entre 0 y 1,
+# y almacena dicho valor en una variable llamada aleatorio
+#from random import *
+aleatorio = random()
+
+#Práctica Random 3
+#Utiliza el método choice() de la librería random para obtener un elemento al azar de la lista de nombres a continuación,
+#y almacena el nombre escogido en una variable llamada sorteo.
+#nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
+
+nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
+sorteo = choice(nombres)
+print(sorteo)
