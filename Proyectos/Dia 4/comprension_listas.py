e1c4c0efcef717eb47857d6ab0c55de13069395e
Emanuel-TC <heberemanuelroma@gmail.com>
2022-11-03 09:26:37 -0600

    _compresion_de_listas_ejercicios

--- Proyectos/Dia 4/comprension_listas.py
+++ Proyectos/Dia 4/comprension_listas.py
@@ -34,5 +34,30 @@ metros =[pie*3.281 for pie in pies]
 print(metros)
 
 
+#Práctica Comprensión de Listas 1
+#Para realizar el ejercicio a continuación, puedes optar por diferentes caminos. Si bien en programación el camino
+# correcto es el que devuelve el resultado correcto, te animo a que intentes aplicar los conceptos de comprensión de listas para comenzar a afianzarlos para el futuro. ¡Pueden resultarte muy útiles en tu práctica profesional!
+#Crea una lista valores_cuadrado formada por los números de la lista valores, elevados al cuadrado.
+#valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores_cuadrado = [cuadrado ** 2 for cuadrado in valores]
+print(valores_cuadrado)
+
+#Crea una lista valores_pares formada por los números de la lista valores que (¡adivinaste!) sean pares.
+#valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores_pares = [par for par in valores if par % 2 ==0]
+print(valores_pares)
+
+#Para la siguiente lista de temperaturas en grados Fahrenheit, expresa estos mismos valores en una nueva lista
+# de valores de temperatura en grados Celsius. La conversión entre tipo de unidades es la siguiente:
+#°C = (°F - 32) * (5/9)
+#o expresado de otro modo:
+# (grados_fahrenheit-32)*(5/9)
+
+temperatura_fahrenheit = [32, 212, 275]
+grados_celsius = [(grado -32)*(5/9) for grado in temperatura_fahrenheit]
+print(grados_celsius)
+
 
 
