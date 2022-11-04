eb69ee4f0fc22d710b0394e453e2c3ccc15327d8
Emanuel-TC <heberemanuelroma@gmail.com>
2022-11-03 22:18:07 -0600

    _proyecto_dia_4_juego_numero_correciones

--- Proyectos/Dia 4/_poyecto_dia_4_juego_numero.py
+++ Proyectos/Dia 4/_poyecto_dia_4_juego_numero.py
@@ -1,6 +1,6 @@
 from random import randint
 intentos = 0
-
+numero = 0
 numero_secreto = randint(1,100)
 nombre = input("Dime tu nombre: \n")
 print(f"Hola {nombre}, estoy pensando un numero entre el 1 y el 100")
@@ -9,12 +9,13 @@ print("Tienes 8 intentos para averiguarlo")
 while intentos < 8:
     numero = int(input("Ingresa un numero entre el 1 y el 100: "))
     intentos += 1
-
-    if numero < numero_secreto:
+    if numero not in range(1,100):
+        print(f"El número {numero} no es válido, ingresa un número entre el 1 y el 100")
+    elif numero < numero_secreto:
         print(f"El numero que estoy pensando es mayor a {numero}")
-    if numero > numero_secreto:
+    elif numero > numero_secreto:
         print(f"El numero que estoy pensando es menor a {numero}")
-    if numero == numero_secreto:
+    else:
         print(f"Qué crack, el numero que estoy pensando sí es {numero}, adivinaste en {intentos} intentos")
         break
 if numero != numero_secreto:
