bf60bdb6b7e1e0d2c2aaf36a85dd63be5b6bd475
Emanuel-TC <heberemanuelroma@gmail.com>
2022-11-08 15:10:27 -0600

    _funciones_dinamicas_ejercios

--- Proyectos/Dia 5/funciones_dinamicas.py
+++ Proyectos/Dia 5/funciones_dinamicas.py
@@ -37,3 +37,55 @@ def revisa_3_numeros(lista):
 
 resultado = revisa_3_numeros([10,200,500])
 print(resultado)
+
+''' Ejercicio 1:
+    Crea una función (todos_positivos) que reciba una lista de números como parámetro, y devuelva True si todos los 
+    valores de una lista son positivos, y False si al menos uno de los valores es negativo. Crea una lista llamada 
+    lista_numeros con valores positivos y negativos.
+
+No invoques la función, solo es necesario definirla.
+'''
+def todos_positivos(lista_numeros):
+    for numero in lista_numeros:
+        if numero < 0:
+            return False
+        else:
+            pass
+    return True
+lista_numeros = [1,2,3,4,5,6,7,8,9,-1]
+
+resultado = todos_positivos(lista_numeros)
+print(resultado)
+
+''' Ejercicio 2:
+Crea una función (suma_menores) que sume los números de una lista (almacenada en la variable lista_numeros) 
+siempre y cuando sean mayores a 0 y menores a 1000, y devuelva el resultado de dicha suma.
+    '''
+def suma_menores(lista_numeros):
+    suma = 0
+    for numero in lista_numeros:
+        if numero in range(1,1000):
+            suma += numero
+        else:
+            pass
+    return suma
+lista_numeros = [1,2,3,10000,-3]
+
+resultado = suma_menores(lista_numeros)
+print(resultado)
+
+''' Ejercicio 3:
+    Crea una función (cantidad_pares) que cuente la cantidad de números pares que existen 
+    en una lista (lista_numeros), y devuelva el resultado de dicha cuenta.
+    '''
+def cantidad_pares(lista_numeros):
+    cuenta = 0
+    for numero in lista_numeros:
+        if numero % 2 == 0:
+            cuenta += 1
+        else:
+            pass
+    return cuenta
+lista_numeros = [1,2,3,4,5,6,7]
+resultado = cantidad_pares(lista_numeros)
+print(resultado)
