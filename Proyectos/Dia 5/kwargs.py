21a0bc6f44d9cfc2fb8b68c0b1c7759e4fb8707a
Emanuel-TC <heberemanuelroma@gmail.com>
2022-11-09 14:35:52 -0600

    _kwargs_ejercios

--- Proyectos/Dia 5/kwargs.py
+++ Proyectos/Dia 5/kwargs.py
@@ -19,3 +19,52 @@ args =[1,2,3,4,5,6,7,8,9,10]
 kwargs ={"a":1,"b":2,}
 
 prueba(1,2,*args,**kwargs)
+
+''' Ejercicio 1:
+Crea una función llamada cantidad_atributos que cuente la cantidad de parémetros 
+que se entregan, y devuelva esa cantidad como resultado.
+'''
+def cantidad_atributos(**kwargs):
+    contador = 0
+    for kwarg in kwargs:
+        contador += 1
+    return contador
+print(cantidad_atributos(a=1, b=2, c=0))
+
+''' Ejercicio 2:
+Crea una función llamada lista_atributos que devuelva en forma de lista los valores de los atributos entregados 
+en forma de palabras clave (keywords). La función debe preveer recibir cualquier cantidad de argumentos de este tipo.
+'''
+def lista_atributos(**kwargs):
+    lista = []
+    for clave, valor in kwargs.items():
+        lista.append(valor)
+    return lista
+kwargs = {"a":1,"b":2,"c":3,"d":4}
+print(lista_atributos(**kwargs))
+
+''' Ejercicio 3:
+    Crea una función llamada describir_persona, que tome como parámetros su nombre y luego una cantidad 
+    indetermida de argumentos. Esta función deberá mostrar en pantalla:
+
+Características de {nombre}:
+{nombre_argumento}: {valor_argumento}
+{nombre_argumento}: {valor_argumento}
+etc...
+Por ejemplo:
+
+describir_persona("María", color_ojos="azules", color_pelo="rubio")
+
+Mostrará en pantalla:
+
+Características de María:
+color_ojos: azules
+color_pelo: rubio
+    '''
+
+def describir_persona(nombre, **kwargs):
+    print(f"Características de {nombre}:")
+    for clave, valor in kwargs.items():
+        print(f"{clave} : {valor}")
+
+describir_persona("María", color_ojos="azules", color_pelo="rubio")
