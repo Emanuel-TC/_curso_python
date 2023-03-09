56b91c7951e74f76ee2a629e730ec8db2009a547
emanuel <heberemanuelroma@gmail.com>
2023-03-08 22:36:20 -0600

    clases_y_atributos

---
+++ Proyectos/Dia 7/atributos.py
@@ -0,0 +1,64 @@
+class Pajaro:
+    #atributos de clase
+    alas = True
+
+    #atributos de instancia
+    def __init__(self,color,especie):
+        self.color = color
+        self.especie = especie
+
+mi_pajaro = Pajaro("negro","Tucán")
+
+if mi_pajaro.alas:
+    print(f"Mi pájaro sí tiene alas y es un {mi_pajaro.especie} con color {mi_pajaro.color}")
+else:
+    print(f"Mi pájaro no tiene alas y es un {mi_pajaro.especie} con color {mi_pajaro.color}")
+
+'''
+Práctica Atributos 1
+Crea una clase llamada Casa, y asígnale atributos: color, cantidad_pisos.
+Crea una instancia de Casa, llamada casa_blanca, de color "blanco" y cantidad de pisos igual a 4.
+'''
+
+
+class Casa:
+    def __init__(self, color, cantidad_pisos):
+        self.color = color
+        self.cantidad_pisos = cantidad_pisos
+
+casa_blanca = Casa("blanco", 4)
+
+'''
+Práctica Atributos 2
+Crea una clase llamada Cubo, y asígnale el atributo de clase:
+    caras = 6
+y el atributo de instancia:
+    color
+Crea una instancia cubo_rojo, de dicho color.
+'''
+class Cubo:
+    caras = 6
+
+    def __init__(self, color):
+        self.color = color
+
+cubo_rojo = Cubo("rojo")
+
+'''
+Práctica Atributos 3
+Crea una clase llamada Personaje, y asígnale el siguiente atributo de clase:
+    real = False
+Crea una instancia llamada harry_potter con los siguientes atributos de instancia:
+    especie = "Humano"
+    magico = True
+    edad = 17
+'''
+class Personaje:
+    real = False
+
+    def __init__(self, especie, magico, edad):
+        self.especie = especie
+        self.magico = magico
+        self.edad = edad
+
+harry_potter = Personaje("Humano", True, 17)
---
+++ Proyectos/Dia 7/clases.py
@@ -0,0 +1,36 @@
+class Pajaro:
+    pass
+tucan = Pajaro()
+perico = Pajaro()
+
+print(tucan, perico)
+'''
+Crea una clase llamada Personaje y a continuación, crea un objeto a partir de ella, 
+por ejemplo: harry_potter
+'''
+class Personaje:
+    pass
+harry_potter = Personaje()
+
+'''
+Crea una clase llamada Dinosaurio, y tres instancias a partir de ella: 
+velociraptor, tiranosaurio_rex y braquiosaurio .
+'''
+class Dinosaurio:
+    pass
+
+velociraptor = Dinosaurio()
+tiranosaurio_rex = Dinosaurio()
+braquiosaurio = Dinosaurio()
+
+'''
+Crea una clase llamada PlataformaStreaming y crea los siguientes objetos a partir de ella: 
+netflix, hbo_max, amazon_prime_video
+'''
+
+class PlataformaStreaming:
+    pass
+
+netflix = PlataformaStreaming()
+hbo_max = PlataformaStreaming()
+amazon_prime_video = PlataformaStreaming()
