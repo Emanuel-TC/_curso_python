29b15c32b8f1bf21819d98ccad6488dbefdb690d
emanuel <heberemanuelroma@gmail.com>
2023-03-12 19:02:01 -0600

    Modulos y submodulos

---
+++ Proyectos/Dia 8/mi_paquete/ejercicio.py
@@ -0,0 +1,6 @@
+from paquete_ema import suma_y_resta
+from paquete_ema.subpaquete import saludo
+mi_resta = suma_y_resta.resta(15,2)
+mi_suma = suma_y_resta.suma(15,2)
+print(mi_resta,mi_suma)
+saludo.saludar()
