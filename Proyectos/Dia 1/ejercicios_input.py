0cf3a10c09c9737d47e98080aa2724898ccd20f3
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 15:13:01 -0600

    Ejercicios para entender el funcionamiento de la función input

---
+++ Proyectos/Dia 1/ejercicios_input.py
@@ -0,0 +1,27 @@
+# Podemos agregar datos mediante la función 'input', pero también podemos imrpimir los datos ingresados en una impresión
+
+print("Tu nombre es: " + input("Dime tu nombre:\n"))
+
+'''Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+¿Qué estás estudiando?
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).'''
+
+print(input("¿Qué estás estudiando?"))
+
+'''
+Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+
+¿En qué país vives?
+
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+'''
+print(input("¿En qué país vives?"))
+
+'''
+Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+
+Escribe tu nombre:
+Escribe tu apellido:
+El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+'''
+print("Tu nombre es: "+ input("Escribe tu nombre") + " " + input("Escribe tu apellido"))
