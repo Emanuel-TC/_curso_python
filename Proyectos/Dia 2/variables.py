8c6d5b67747cc044bbde08fbec3cdf638bc87dba
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 15:43:05 -0600

    Eejercicios de práctica de variables

--- Proyectos/Dia 2/variables.py
+++ Proyectos/Dia 2/variables.py
@@ -1,2 +1,45 @@
+'''
+Las variables nos permiten almacenar datos para usarlos posteriormente
+'''
+# En esta variable almacenamos strings
 nombre = input("Dime tu nombre: ")
 print("Tu nombre es: " + nombre)
+# En esta variable almacenamos un int
+edad = 19
+print(edad)
+
+'''
+Práctica con Variables 1
+Declara dos variables, llamadas nombre y edad.
+
+Asigna a la variable nombre el valor "Tony Soprano", y a la edad, el valor 51.
+'''
+nombre = "Tony Soprano"
+edad = 51
+
+'''
+Práctica con Variables 2
+Crea tres variables:
+
+nombre
+
+apellido
+
+nombrecompleto
+
+A nombre, asígnale el valor "Julia", y en apellido, asigna el valor "Roberts". Finalmente, construye la variable 
+nombre completo concatenando las variables (recuerda sumar un espacio intermedio).
+'''
+
+nombre = "Julia"
+apellido = "Roberts"
+nombrecompleto = nombre + " " + apellido
+
+'''
+Práctica con Variables 3
+Declara la variable curso, asígnale el valor "Python", y muestra en pantalla la frase:
+
+Estás tomando un curso de curso
+'''
+curso = "Python"
+print("Estás tomando un curso de " + curso)
