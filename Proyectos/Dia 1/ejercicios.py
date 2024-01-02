e31e9b3d72678efd88ad1c6d8309448cd7e40eed
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 13:44:27 -0600

    Se agregan ejercicios para comprender mejor el uso de la diagonal invertida

---
+++ Proyectos/Dia 1/ejercicios.py
@@ -0,0 +1,12 @@
+'''
+Los siguientes ejercicios son para estudiar el comortamiento de cadenas en python
+'''
+
+# Cadenas con el uso de caracteres especiales
+
+# Usamos la diagonas invertida para poder usar caracteres especiales
+print("Hola, me llamo \"Emanuel\" ")
+# Para saltos línes usamos \n
+print("Hola, mundo, uso la \"\\n\" para hacer un \nsalto de línea")
+# Para tabular usamos \t
+print("Usamos el \"\\t\" para hacer una \ttabulación")
