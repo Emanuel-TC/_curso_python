3e8dc72b6ce8d713650be9bd38bc43ccd38dd754
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 16:00:49 -0600

    Eejercicios de enteros y flotantes

--- Proyectos/Dia 2/Integers_Floats.py
+++ Proyectos/Dia 2/Integers_Floats.py
@@ -1,5 +1,7 @@
+# En este espacio usaremos números enteros y números flotantes
 num1 = 7.5
 num2 = 2.5
 num2 = num2 + num1
 print(num2)
+# Con la función type podemos ver el tipo de dato que es nuestra variable
 print(type(num2))
