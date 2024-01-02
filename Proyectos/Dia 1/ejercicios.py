7fd90284e950dc4e0eed689cb6a06e3ba659c2bd
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 13:45:25 -0600

    Ejercicios de pruebas

--- Proyectos/Dia 1/ejercicios.py
+++ Proyectos/Dia 1/ejercicios.py
@@ -10,3 +10,29 @@ print("Hola, me llamo \"Emanuel\" ")
 print("Hola, mundo, uso la \"\\n\" para hacer un \nsalto de línea")
 # Para tabular usamos \t
 print("Usamos el \"\\t\" para hacer una \ttabulación")
+
+
+'''
+Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+Línea 1
+Línea 2
+Línea 3
+'''
+print("Línea 1\nLínea 2\nLínea 3")
+
+'''
+Crea un código que imprima en pantalla la siguiente expresión.
+A	B	C
+D	E	F
+G	H	I
+'''
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+'''
+Crea un código que imprima en pantalla la siguiente expresión:
+
+Barra Normal: /
+Barra Invertida: \
+'''
+print("Barra normal: /\nBarra Invertida: \\")
