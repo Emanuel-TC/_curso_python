b83632bf6dc9df910858cc0ebe9d8f170b9b67e5
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-02 16:05:58 -0600

    Ejercicios para practicar tipos de datos enteros y flotantes

--- Proyectos/Dia 2/integers_floats.py
+++ Proyectos/Dia 2/integers_floats.py
@@ -5,3 +5,33 @@ num2 = num2 + num1
 print(num2)
 # Con la función type podemos ver el tipo de dato que es nuestra variable
 print(type(num2))
+
+'''Práctica con Integers
+Declara una variable numérica llamada num_entero que contenga un valor de tipo integer de tu elección.
+Imprime el tipo de dato de dicha variable.
+'''
+num_entero = 22
+print(type(num_entero))
+
+'''Práctica con Floats
+Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección.
+Imprime el tipo de dato de dicha variable.
+'''
+num_decimal = 22.0
+print(type(num_decimal))
+
+'''Práctica con Tipos de Datos Numéricos
+¿De qué tipo es el resultado de la suma de 7.5 + 2.5? Genera el código para verificarlo.
+
+Para ello, crea dos variables:
+
+num1 = 7.5
+
+num2 = 2.5
+
+A continuación, muestra en pantalla el tipo de dato que resulta de la suma de ambos números.'''
+
+num1 = 7.5
+num2 = 2.5
+num3 = num2 + num1
+print(type(num3))
