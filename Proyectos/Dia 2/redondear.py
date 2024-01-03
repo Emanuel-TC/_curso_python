76ecf0641e23afc3111b0e8996b0bcd61f006678
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 11:30:47 -0600

    Actualización de comentarios para entender función round

--- Proyectos/Dia 2/redondear.py
+++ Proyectos/Dia 2/redondear.py
@@ -1,4 +1,6 @@
 resultado = 90/7
-redondeo = round(resultado)
-
+redondeo = round(resultado) # round permite redondear el valor al entero más cercano
 print(redondeo)
+# también podemos usarlo para obtener sólo cierta cantidad de decimales, por ejemplo, para dos dígitos:
+n = round(13.98888888888,2)
+print(n)
