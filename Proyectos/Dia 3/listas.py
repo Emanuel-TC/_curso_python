b3c8b3fd6a17e9d4f6fa0d640053b95fea9d6e2f
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 14:13:50 -0600

    Correción de comentarios de listas

--- Proyectos/Dia 3/listas.py
+++ Proyectos/Dia 3/listas.py
@@ -1,21 +1,31 @@
+# En este archivo veremos el uso de listas
 mi_lista = ['a','b','c']
 otra_lista = ['Hola', 12, 7.1]
+# podemos ver la longitud de la lista
 resultado = len(mi_lista)
+# imprimimos el tipo de dato
 print(type(mi_lista))
 print(resultado)
+# podemos ver el elemento de la lista en la posición 0
 resultado = mi_lista[0]
 print(resultado)
+# en la variable guardamos todos los elementos de la lista tomando de uno en uno
 resultado = mi_lista[::1]
 print(resultado)
+# podemos unir dos listas de esta forma
 print(mi_lista+otra_lista)
 mi_lista3 = mi_lista+otra_lista
+# Podemos agregar alementos a la lista, en este caso, en la primer posición, agremamos la cadena "Cabeza"
 mi_lista3[0] = "Cabeza"
 print(mi_lista3)
-mi_lista3.append('cola') #añade un elemento, por defecto, al final de la lista
+# añade un elemento, por defecto, al final de la lista
+mi_lista3.append('cola')
 print(mi_lista3)
-mi_lista3.pop() #por defecto elimina el último, en este caso, eliminaría cola
+# por defecto elimina el último, en este caso, eliminaría cola
+mi_lista3.pop()
 print(mi_lista3)
-mi_lista3.pop(0) #se indica el indice que se va a elminiar, en este caso, cabeza
+# se indica el indice que se va a elminiar, en este caso, cabeza
+mi_lista3.pop(0)
 print(mi_lista3)
 b = mi_lista3.pop(0) #se indica el indice que se va a elminiar, en este caso, se guarda
 print(f"Se eliminó la letra {b} de la lista {mi_lista3}")
@@ -25,9 +35,9 @@ print(nueva_lista)
 nueva_lista.reverse() #ordena la lista del ultimo al primero
 print(nueva_lista)
 
-#agrega el elemento "motocicleta" a la siguiente lista de medios de transporte:
-#medios_transporte = ["avión", "auto", "barco", "bicicleta"]
-#No debes modificar la línea de código ya suministrada, sino que debes emplear el método apropiado de listas para añadir un nuevo elemento.
+'''agrega el elemento "motocicleta" a la siguiente lista de medios de transporte:
+medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+No debes modificar la línea de código ya suministrada, sino que debes emplear el método apropiado de listas para añadir un nuevo elemento.'''
 medios_transporte = ["avión", "auto", "barco", "bicicleta"]
 medios_transporte.append("motocicleta")
 print(medios_transporte)
