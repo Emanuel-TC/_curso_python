463052e9bda093cf5bb927001ab91b9c4a9a7a3b
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 14:05:57 -0600

    Correción de comentarios de propiedades

--- Proyectos/Dia 3/propiedades.py
+++ Proyectos/Dia 3/propiedades.py
@@ -6,25 +6,28 @@ print("agua" in poema) #si existe esa palabra en la variable que contiene esa st
 poema = len(poema) #su longitud en caracteres
 print(f"La longitud de este poema es de {poema} caracteres")
 
-#Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla.
-#Por suerte, conoces que los strings son multiplicables y puedes realizar esta actividad
-# de forma simple y elegante.
+# EJERCICIOS
+
+'''Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla.
+Por suerte, conoces que los strings son multiplicables y puedes realizar esta actividad
+de forma simple y elegante.'''
 texto = "Repetición"
 print(texto*15)
 
-#Verifica si la palabra "agua" no se encuentra en el siguiente haiku. Debes imprimir el booleano.
-#Tierra mojada,
-#mis recuerdos de viaje
-#entre las lluvias
+'''Verifica si la palabra "agua" no se encuentra en el siguiente haiku. Debes imprimir el booleano.
+Tierra mojada,
+mis recuerdos de viaje
+entre las lluvias'''
 
 poema ="""Tierra mojada
 mis recuerdos de viaje,
 entre las lluvias"""
+
 print("agua" not in poema)
 palabras = list(poema.split())
 print(len(palabras))
 
 
-#Muestra en pantalla el largo (en números de caracteres) de la palabra electroencefalografista.
+'''Muestra en pantalla el largo (en números de caracteres) de la palabra electroencefalografista.'''
 palabra = "electroencefalografista"
 print(len(palabra))
