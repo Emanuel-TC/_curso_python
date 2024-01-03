2be7bad651933f51943d2c72588a772e9d3711df
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 17:48:46 -0600

    Se agregan ejercicios de práctica para set

--- Proyectos/Dia 3/mi_set.py
+++ Proyectos/Dia 3/mi_set.py
@@ -31,6 +31,34 @@ sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
 ganador = sorteo.pop()
 print(f"El ganador 'a' es {ganador} ")
 
+# Usando el método clear para limpiar el set
+mi_set.clear()
+print(mi_set)
 # Ejercicios
 
-''''''
+'''Práctica Sets 1
+Une los siguientes sets en uno solo, llamado mi_set_3:
+
+{1, 2, "tres", "cuatro"}
+
+{"tres", 4, 5}'''
+
+mi_set_1 = {1, 2, "tres", "cuatro"}
+mi_set_2 = {"tres", 4, 5}
+mi_set_3 = mi_set_1.union(mi_set_2)
+
+'''Práctica Sets 2
+Elimina un elemento al azar del siguiente set, utilizando métodos de sets.
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}'''
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+ganador = sorteo.pop()
+
+'''Práctica Sets 3
+Agrega el nombre Damián al siguiente set, utilizando métodos de sets:
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}'''
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+sorteo.add("Damián")
