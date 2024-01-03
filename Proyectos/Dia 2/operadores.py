31fba9b20c8dec1441041f1338dd7d4efeb43cd0
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 11:07:41 -0600

    Actualización de comentarios para los operadores matemáticos

--- Proyectos/Dia 2/operadores.py
+++ Proyectos/Dia 2/operadores.py
@@ -1,11 +1,14 @@
 x = 6
 y = 2
 z = 7
-print(f"{x} + {y} = {x+y}")
-print(f"{x} - {y} = {x-y}")
-print(f"{x} * {y} = {x*y}")
-print(f"{x} / {y} = {x/y}")
+print(f"{x} + {y} = {x+y}") #suma
+print(f"{x} - {y} = {x-y}") #resta
+print(f"{x} * {y} = {x*y}") #multiplicación
+print(f"{x} / {y} = {x/y}") # división
+# Las siguientes operaciones son útiles para resolver operaciones más complejas
+# La división al piso elimina los deecimales del resultado de la divisón
 print(f"{z} // {y} = {z//y}") #division al piso
+# El módulo es el residuo de una división
 print(f"{z} modulo de {y} es : {z%y}") #modulo
 print(f"{x} elevado a la {y} es: {x**y}")
 print(f"{x} elevado a la {3} es: {x**3}")
