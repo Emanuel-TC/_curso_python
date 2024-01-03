96ff53469c3c17a699c7708003800d01d73e14c1
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 14:15:47 -0600

    Se agregan ejercicios de prática de las listas

--- Proyectos/Dia 3/listas.py
+++ Proyectos/Dia 3/listas.py
@@ -35,9 +35,28 @@ print(nueva_lista)
 nueva_lista.reverse() #ordena la lista del ultimo al primero
 print(nueva_lista)
 
+'''Práctica Listas 1
+Crea una lista con 5 elementos, dentro de la variable mi_lista. Puedes incluir strings, booleanos, números, etc.'''
+mi_lista = ['a', 'b',3, 4.0, "Cola"]
+
 '''agrega el elemento "motocicleta" a la siguiente lista de medios de transporte:
 medios_transporte = ["avión", "auto", "barco", "bicicleta"]
 No debes modificar la línea de código ya suministrada, sino que debes emplear el método apropiado de listas para añadir un nuevo elemento.'''
 medios_transporte = ["avión", "auto", "barco", "bicicleta"]
 medios_transporte.append("motocicleta")
 print(medios_transporte)
+
+'''Práctica Listas 3
+Utiliza el método pop() para quitar el tercer elemento de la siguiente lista llamada frutas, y almacénalo en una variable llamada eliminado. Utiliza métodos de listas sin alterar la línea de código ya suministrada.
+
+manzana
+
+banana
+
+mango
+
+cereza
+
+sandía'''
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
