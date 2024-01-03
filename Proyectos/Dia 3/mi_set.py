042e2030f84fb8cb95c1f46b03b52c85a1989c5d
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 17:47:22 -0600

    Correción de comentarios de set

--- Proyectos/Dia 3/mi_set.py
+++ Proyectos/Dia 3/mi_set.py
@@ -1,29 +1,36 @@
-mi_set = set([1,2,3,4,5]) #primer forma de declarar el set
+# primer forma de declarar el set
+mi_set = set([1,2,3,4,5])
 print(type(mi_set))
 print(mi_set)
 
 otro_set = {1,2,3}
 print(type(otro_set))
 print(otro_set)
+# Segunda forma de declarar un set
 otro_set = {1,2,3,4,1,1,1,1,2,2,2,2}
 print(f"En los sets no hay repetcion de elementos, como en este {otro_set}")
 otro_set = {1,2,3,4,1,1,1,1,2,2,2,2,(1,2,3)}
 print(f"Pero si puede tener tuples dentro, como este {otro_set}")
 print(f"El largo del set {otro_set} es {len(otro_set)}")
+# podemos verificar si un elemento en específico existe en el set
 print(f"¿Es verdad que 2 está en {otro_set}? {2 in otro_set}")
-#usamos métodos de set
+# usamos métodos de set
 un_set_mas = mi_set.union(otro_set)
 print(f"Los valores del nuevo set son : {un_set_mas}")
-#metdo agregar
+# metdo agregar
 un_set_mas.add(6)
 print(f"Los valores del nuevo set son : {un_set_mas}")
-#metodo eliminiar
+# metodo eliminiar
 un_set_mas.remove(1)
 print(f"Los valores del nuevo set son : {un_set_mas}")
-#remover un elemento de manera aleatoria
-sorteo= un_set_mas.pop() #siempre y cuando se deje vacío el argumento
+# remover un elemento de manera aleatoria
+sorteo = un_set_mas.pop() #siempre y cuando se deje vacío el argumento
 print(f"El ganador es: {sorteo}")
 
 sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
 ganador = sorteo.pop()
 print(f"El ganador 'a' es {ganador} ")
+
+# Ejercicios
+
+''''''
