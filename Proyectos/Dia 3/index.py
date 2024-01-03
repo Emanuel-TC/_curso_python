21d807d6c504e748fa07e40978db10a028fbcb06
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-03 12:01:22 -0600

    Actualización de cometarios del método index

--- Proyectos/Dia 3/index.py
+++ Proyectos/Dia 3/index.py
@@ -1,3 +1,4 @@
+# En este archivo vamos a aprender a usar el método index
 mi_texto = "Esta es una prueba"
 resultado = mi_texto[5]
-print(resultado)
+print(f"La leta en la posición 5, del texto '{mi_texto}' es: {resultado}, y la palabra 'prueba', comienza en la posición {mi_texto.index('prueba')}")
