55306fad48187ae937e62e3516984de022ed18cd
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-05 13:35:03 -0600

    Correciones del proyecto del día 3.

--- Proyectos/Dia 3/proyecto_dia_3.py
+++ Proyectos/Dia 3/proyecto_dia_3.py
@@ -18,20 +18,26 @@ aparicion_letra_2 = nuevo_texto.count(letra_2) # se almacena la cantidad de vece
 aparicion_letra_3 = nuevo_texto.count(letra_3) # que aparece cada letra en el texto
 
 numero_de_palabras = list(texto.split()) #esta función separa el texto en elementos separados regularmente por un espacio " " y se almacena en unavaribale en forma de lista
-if "." in numero_de_palabras:
-    numero_de_palabras.pop()
+if "." in numero_de_palabras[-1]:
+    ultima_palabra = numero_de_palabras[-1]
+    ultima_palabra = ultima_palabra.split(".")
+    print(f"Esta es la lista de la última palabra: {ultima_palabra}")
+    ultima_palabra = ultima_palabra[0]
+    print(f"La última palbra es {ultima_palabra}")
+else:
+    ultima_palabra = numero_de_palabras[-1]
 primer_letra = texto[0] #Solicito valor del primer índice del texto original
 ultima_letra = texto[-1] #ultimo valor del índice del texto original
 texto_inverso = texto[::-1] # se invierte todo el texto desde : valor 0 : hasta últo valor :-1 en reversa
-buscar_python = 'Python' in texto #almacenamos un True o un False con la comparación
+buscar_python = ('Python' in texto) or ('python' in texto) #almacenamos un True o un False con la comparación
 dic = {True:"sí", False:"no"} #construyo un diccionario con dos valores, sí y no, con propiedad True o False
 print(f"""En tu texto: '{texto}' \nse encuentran {aparicion_letra_1} veces la letra {letra_1},
               {aparicion_letra_2} veces la letra {letra_2}, 
               y {aparicion_letra_3} veces la letra {letra_3}.             
               Además existen {len(numero_de_palabras)} palabras en total.
-              La primer letras es {primer_letra}, lo que corresponde a la inicial
-              de la palabra {numero_de_palabras[0]} y la ultima letra es {ultima_letra}, que corresponde a la última letra de la
-              palabra {numero_de_palabras[-1]}.
+              La primer letra es {primer_letra}, lo que corresponde a la inicial
+              de la palabra {numero_de_palabras[0]} y la ultima letra es {ultima_palabra[-1]}, que corresponde a la última letra de la
+              palabra {ultima_palabra}.
               El texto al revés dice: '{texto_inverso}'
               Finalmente la palabra 'Python' {dic[buscar_python]} está en el texto""")
 
