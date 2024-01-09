5e04fd11d80f25cbee469f87263deee57b6c90dc
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-09 10:57:03 -0600

    Se corrijen comentarios de ejercicios

--- Proyectos/Dia 4/_random.py
+++ Proyectos/Dia 4/_random.py
@@ -1,5 +1,5 @@
 #from random import randint
-#para importar todos los metodos de random,después de import escribimos un asterisco
+# para importar todos los metodos de random,después de import escribimos un asterisco
 from random import *
 aleatorio = randint(1,50) #randint es para un valor random pero del tipo entero
 print(aleatorio)
@@ -23,22 +23,22 @@ numeros = list(range(5,50,5))
 shuffle(numeros)
 print(numeros)
 
-#Implementa la función randint() de la librería random que te permita obtener un número entero del 1 al 10,
-# y almacena dicho valor en una variable llamada aleatorio
+'''Implementa la función randint() de la librería random que te permita obtener un número entero del 1 al 10,
+ y almacena dicho valor en una variable llamada aleatorio'''
 
-#from random import randint
+# from random import randint
 aleatorio = randint(1,10)
 
-#Práctica Random 2
-#Implementa la función random() de la librería random que te permita obtener un número decimal entre 0 y 1,
-# y almacena dicho valor en una variable llamada aleatorio
-#from random import *
+'''Práctica Random 2
+Implementa la función random() de la librería random que te permita obtener un número decimal entre 0 y 1,
+ y almacena dicho valor en una variable llamada aleatorio
+from random import *'''
 aleatorio = random()
 
-#Práctica Random 3
-#Utiliza el método choice() de la librería random para obtener un elemento al azar de la lista de nombres a continuación,
-#y almacena el nombre escogido en una variable llamada sorteo.
-#nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
+'''Práctica Random 3
+Utiliza el método choice() de la librería random para obtener un elemento al azar de la lista de nombres a continuación,
+y almacena el nombre escogido en una variable llamada sorteo.
+nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]'''
 
 nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
 sorteo = choice(nombres)
