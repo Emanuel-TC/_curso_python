3c2cdac0ab4bb07636f8f36c8ac805f0ccb17251
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-10 13:25:42 -0600

    Correción de manejo de ruta usando el método getcwd de os y pathlib para usar cualquier ruta

--- Proyectos/Dia 6/_path_lib.py
+++ Proyectos/Dia 6/_path_lib.py
@@ -1,10 +1,14 @@
 from pathlib import Path, PureWindowsPath
+import os
+
+ruta_actual = os.getcwd()
+#print(ruta_actual)
 
 
-ruta = Path("/Users/Emanuel/Desktop/_curso_python/Proyectos/Dia 6/prueba.txt") #ruta para cualquoer sistema
+ruta = Path(ruta_actual+"/prueba.txt") #ruta para cualquoer sistema
 ruta_windows = PureWindowsPath(ruta) #convierte la ruta al estilo windows
 if not ruta.exists():
     print("File not found")
 else:
-    print(f"File found in {ruta_windows}")
+    print(f"File found in {ruta_windows}\nEl archivo dice lo siguiente:\n")
     print(ruta.read_text())
