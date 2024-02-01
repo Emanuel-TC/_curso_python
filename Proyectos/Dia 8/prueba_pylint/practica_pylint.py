59bc86cc335ace143fbc462cda68b62a3652a38a
Emanuel-TC <heberemanuelroma@gmail.com>
2024-01-31 18:55:22 -0600

    Correción de práctica de pylint y se agrega la línea de comando paara ejecutar pylint

--- Proyectos/Dia 8/prueba_pylint/practica_pylint.py
+++ Proyectos/Dia 8/prueba_pylint/practica_pylint.py
@@ -1,11 +1,18 @@
-'''
-funcion para sumar
-'''
+"""
+Funcion para sumar
+"""
 
 
 def sumar(numero1, numero2):
+
+    """
+        Esta función recibe dos argumentos numéricos
+        y devuelve su suma
+        """
+
     return numero1+numero2
 
 
 SUMA = sumar(5, 7)
-print(SUMA)
+
+print(f'El resultado de la suma fue: {SUMA}')
