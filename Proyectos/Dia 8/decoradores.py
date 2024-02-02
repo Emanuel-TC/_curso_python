7117613d96024f1a2943916c082e52a320e0d7d8
Emanuel-TC <heberemanuelroma@gmail.com>
2024-02-02 12:12:04 -0600

    Se ajustan comentarios patra entender los usos de los decoradores

--- Proyectos/Dia 8/decoradores.py
+++ Proyectos/Dia 8/decoradores.py
@@ -1,3 +1,29 @@
+"""
+Entendiendo el concepto de las funciones como objeto
+"""
+
+def cambiar_letras(tipo):
+
+    """En esta función recibimos un tipo de opción, may para mayúsculas
+    o min para minúsculas. A partir de eso, devolvemos la función seleccionada y se almcenará en una variable
+    para ser usada
+    """
+    def minusculas(texto):
+        print(texto.lower())
+
+    def mayusculas(texto):
+        print(texto.upper())
+
+    if tipo == "may":
+        return mayusculas  # se regresa como variable sin los () pafra usarse después
+
+    elif tipo == "min":
+        return minusculas  # se regresa como variable sin los () pafra usarse después
+
+
+operacion = cambiar_letras("may")
+operacion("Texto en mayusculas")
+
 def decorar_saludo(funcion):
 
     def otra_funcion(palabra):
@@ -6,6 +32,7 @@ def decorar_saludo(funcion):
         print("Adiós")
     return otra_funcion
 
+# Podemos usar la función decoradora simplemente poniendo el @ y el nombre de la funicón sobre la función a decorar
 #@decorar_saludo
 def mayusculas(texto):
     print(texto.upper())
