cca8817ed96686b3799d28d1ce18146554c307fc
Emanuel-TC <heberemanuelroma@gmail.com>
2024-02-16 18:04:52 -0600

    Extarer elementos de clases

--- Proyectos/Dia 11/web_scraping.py
+++ Proyectos/Dia 11/web_scraping.py
@@ -7,6 +7,13 @@ url = 'https://escueladirecta-blog.blogspot.com'
 resultado = requests.get(url)
 
 sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+titulo = sopa.select('title')[0].getText()
 
 # Usamos el método select para buscar por las etiquetas de html, y recibimos una lista con los resultados, usamos [0] para obtener el primer resultado, y con getText() obtenemos el texto
-print(sopa.select('title')[0].getText())
+print(f"El título del sitio web es: {titulo}")
+
+# extraer elementos de una clase
+columna_perfil = sopa.select('.profile-link')
+for enlace in columna_perfil:
+    print(enlace.getText())
+#print(columna_perfil)
