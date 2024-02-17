f48c425fa1a747b7b7f73780f2312df3509d4559
Emanuel-TC <heberemanuelroma@gmail.com>
2024-02-17 13:01:19 -0600

    Configuración de tamaño,título y fondo de la ventana de la aplicación

--- Proyectos/Dia 12/mi_restaurante.py
+++ Proyectos/Dia 12/mi_restaurante.py
@@ -3,3 +3,20 @@ from tkinter import *
 ''' Este programa realizará la gestión de un restaurante usando la librería tkinter'''
 
 
+# iniciar tkinter
+aplicacion = Tk()
+
+# tamaño de la ventana
+aplicacion.geometry('1200x630+0+0')
+
+# evitar que la ventana se redimensione
+aplicacion.resizable(False, False)
+
+# título de la ventana
+aplicacion.title('Mi Restaurante - Sistema de Facturación')
+
+# color de fondo de la ventana
+aplicacion.config(bg='light blue')
+
+# evitar que la ventana se cierre
+aplicacion.mainloop()
