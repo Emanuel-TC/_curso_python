9a9aa4c48951355e0a7f87e0d5ad1e474ea0a71f
Emanuel <heberemanuelroma@gmail.com>
2025-03-17 22:43:41 +0100

    [Update] New files added. Dics in python

--- Proyectos/pruebas/proyecto_dia3_rehecho/diccionarios.py
+++ Proyectos/pruebas/proyecto_dia3_rehecho/diccionarios.py
@@ -21,7 +21,25 @@ clientes = {    'cliente1': {
 print(f"El cliente {clientes['cliente1']['nombre']} {clientes['cliente1']['apellido']} tiene {clientes['cliente1']['edad']} años")
 print(f"El cliente {clientes['cliente2']['nombre']} {clientes['cliente2']['apellido']} tiene {clientes['cliente2']['edad']} años")
 
-dic = { 'c1:': ['a', 'b', 'c'],
+dic = { 'c1': ['a', 'b', 'c'],
         'c2': ['d', 'e', 'f']
 }
+# Imprime la letra 'e' en mayuscula
 print(dic['c2'][1].upper())
+
+# agregamos elementos al diccionario
+dic['c3'] = ['g', 'h', 'i']
+print(dic['c3'])
+
+# Modificamos un valor del diccionario
+for i in range(3):
+    dic['c3'][i] = dic['c3'][i].upper()
+print(dic['c3'])
+
+# imprimimos los valores del diccionario
+for i in dic.values():
+    print(i)
+
+# imprimimos todo el diccionario en un bucle
+for clave, valor in dic.items():
+    print(clave, valor)
