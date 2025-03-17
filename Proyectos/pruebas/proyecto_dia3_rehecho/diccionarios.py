9b20b45126ce4f919d901fbefd3b0602cae04834
Emanuel <heberemanuelroma@gmail.com>
2025-03-17 22:32:03 +0100

    [Update] New files added

---
+++ Proyectos/pruebas/proyecto_dia3_rehecho/diccionarios.py
@@ -0,0 +1,21 @@
+# Ahora vamos a usar un diccionario
+diccionario = {'c1': 'valor1',
+                'c2': 'valor1',
+                'c3': 'valor3'}
+# print(diccionario['c1'])
+
+clientes = {    'cliente1': {
+                            'nombre': 'Juan',
+                            'apellido': 'Perez',
+                            'dni': '12345678',
+                            'edad': 30
+                        },
+                'cliente2':{
+                            'nombre': 'Ana',
+                            'apellido': 'Gomez',
+                            'dni': '87654321',
+                            'edad': 25
+                            }
+}
+print(f"El cliente {clientes['cliente1']['nombre']} {clientes['cliente1']['apellido']} tiene {clientes['cliente1']['edad']} años")
+print(f"El cliente {clientes['cliente2']['nombre']} {clientes['cliente2']['apellido']} tiene {clientes['cliente2']['edad']} años")
