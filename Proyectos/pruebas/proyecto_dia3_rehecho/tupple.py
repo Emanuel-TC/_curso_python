6204375ee791a8c8ae77a8e4507945932960ea0f
Emanuel <heberemanuelroma@gmail.com>
2025-03-25 22:38:41 +0100

    [update]New data updated

---
+++ Proyectos/pruebas/proyecto_dia3_rehecho/tupple.py
@@ -0,0 +1,11 @@
+# Ocupan menos espacio de memoria
+# No se pueden modificar
+# Son más rápidas
+# Son más seguras
+
+mi_tupple = (1,2,3,4,5)
+# se puede indexar
+print(mi_tupple[0])
+# se puede anidar
+mi_tupple = (1,2,3,4,5, (6,7,8))
+print(mi_tupple[5][1])
